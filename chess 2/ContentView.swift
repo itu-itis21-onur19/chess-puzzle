00e36c932c227c7440d7b1751da8b6370c7c6272
Yasin Onur <onur23@itu.edu.tr>
2025-05-23 20:32:42 +0300

    Initial Commit

---
+++ chess 2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  chess 2
+//
+//  Created by Yasin Onur on 23.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ chess 2/chess_2App.swift
@@ -0,0 +1,17 @@
+//
+//  chess_2App.swift
+//  chess 2
+//
+//  Created by Yasin Onur on 23.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct chess_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
